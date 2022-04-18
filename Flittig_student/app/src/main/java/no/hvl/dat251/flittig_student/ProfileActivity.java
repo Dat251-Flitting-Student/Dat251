9d48ce56297f7632770a47855b982644f823bc6d
Sol Marie <solmarie007@hotmail.com>
2022-04-18 16:30:32 +0200

    Able to increment points, get points and set points. Still have not figured out how to get the total amount of points.
    The frontend needs some enhancement.

--- Flittig_student/app/src/main/java/no/hvl/dat251/flittig_student/ProfileActivity.java
+++ Flittig_student/app/src/main/java/no/hvl/dat251/flittig_student/ProfileActivity.java
@@ -4,51 +4,44 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.provider.ContactsContract;
 import android.util.Log;
-import android.widget.Button;
 import android.widget.TextView;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
-import org.w3c.dom.Text;
-
 public class ProfileActivity extends AppCompatActivity {
     /*
-   Methods for the points given for the specific user and their profile page.
+    This class contains the behaviour for the points as well as displaying user info.
    The information needed to get the different fields are found in UserInfo.
      */
 
     private static final String TAG = "ProfileActivity";
-    private int points = -1;
+    private int points = 0;
 
-    public void setPoints(int value) {
-        // change this so that the users get the correct amount of points.
-        // this have to be updated ? idk, i am tired
+    public void setCurrentPoints(int value) {
+        // Set points for the user.
         FirebaseDatabase database = FirebaseDatabase.getInstance();
         DatabaseReference myRef = database.getReference(UserInfo.getUID());
 
         myRef.setValue(value);
     }
 
-    public void incrementPoints(){
-        //TODO: implement this method.
-
-        // get current points from database (not eventlistener, but read value once)
-        // Check that the received value is valid.
-        // Increment the received value after validation.
-        // Submit (overwrite?) the current value in the database
+    public void incrementCurrentPoints(){
+        // increment the points of the user.
+        int points = getCurrentPoints();
+        if (points > 0) {
+            setCurrentPoints(points + 1);
+        }
     }
 
-   /* public int getPoints() {
+    public int getCurrentPoints() {
+        // Get the current points from the user (only once).
         FirebaseDatabase database = FirebaseDatabase.getInstance();
         DatabaseReference myRef = database.getReference(UserInfo.getUID());
 
@@ -67,27 +60,32 @@ public class ProfileActivity extends AppCompatActivity {
         return points;
     }
 
-    */
+    //TODO: implement a method that keeps track of all the points they have earned in total.
+    public int pointsInTotal() {
+        // lagre som json objekt?
+        return 0;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        // This is what happens when this activity is activated.
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
 
-        //setPoints(101);
-        //System.out.println("The points: " + getPoints());
+        // TODO: add profile picture
 
+        // display the username
         TextView username = (TextView)findViewById(R.id.username);
         username.setText("Navn: " + UserInfo.getUsername());
 
+        // display the school
         TextView school = (TextView)findViewById(R.id.school);
         school.setText("Skole: " + UserInfo.school());
 
+
+        // Get the points from the database, updated automatically.
         FirebaseDatabase database = FirebaseDatabase.getInstance();
         DatabaseReference myRef = database.getReference(UserInfo.getUID());
-
-        //TODO: add profile picture
-
         // Read from the database
         myRef.addValueEventListener(new ValueEventListener() {
             @Override
@@ -96,7 +94,6 @@ public class ProfileActivity extends AppCompatActivity {
                 // whenever data at this location is updated.
                 try {
                     String value = dataSnapshot.getValue().toString();
-                    System.out.println(value + " sol er fin");
                     if (value != null) {
                         Log.d(TAG, "Value is: " + value);
                         TextView points = (TextView) findViewById(R.id.points);
@@ -104,8 +101,8 @@ public class ProfileActivity extends AppCompatActivity {
                     }
                 }
                 catch (NullPointerException ex){
-                    //if the user does not have any points, set them to 0.
-                    setPoints(0);
+                    //if the user does not have any points from before, set them to 0.
+                    setCurrentPoints(0);
                     ex.printStackTrace();
                 }
             }
