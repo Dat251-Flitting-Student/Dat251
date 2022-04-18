69e3a630b4189fa44b44eff18c5ef9355736f01e
vildekristine99 <578051@stud.hvl.no>
2022-04-18 14:04:41 +0200

    fix

--- Flittig_student/app/src/main/java/no/hvl/dat251/flittig_student/MainActivity.java
+++ Flittig_student/app/src/main/java/no/hvl/dat251/flittig_student/MainActivity.java
@@ -42,7 +42,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_signin);
+        setContentView(R.layout.activity_main);
 
         signInButton = findViewById(R.id.sign_in_button);
         mAuth = FirebaseAuth.getInstance();
